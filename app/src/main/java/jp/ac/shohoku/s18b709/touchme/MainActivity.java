198895fdf0891b7906bf6d252d13641cd6901a71
s18b708 <s18b709@shohoku.ac.jp>
2019-11-14 14:18:28 +0900

    sy!

--- app/src/main/java/jp/ac/shohoku/s18b709/touchme/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/s18b709/touchme/MainActivity.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.app.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
