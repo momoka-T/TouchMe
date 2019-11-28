2aec7a18092d5a22681c994c0b9021be628e2908
s18b708 <s18b709@shohoku.ac.jp>
2019-11-28 15:33:26 +0900

    mainactivity競

--- app/src/main/java/jp/ac/shohoku/s18b709/touchme/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/s18b709/touchme/MainActivity.java
@@ -70,7 +70,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.view2);
 
         //音楽開始ボタン
         Button buttonStart = findViewById(R.id.start);
