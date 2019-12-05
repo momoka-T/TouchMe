ba8d25c0c8363fc064af7047b6305d2f68f7e3d0
momokaT <s18b709@shohoku.ac.jp>
2019-12-05 14:45:22 +0900

    ボタンの機能重複のエラー修正

--- app/src/main/java/jp/ac/shohoku/s18b709/touchme/MainActivity.java
+++ app/src/main/java/jp/ac/shohoku/s18b709/touchme/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_view);
             // ボタンを押したときにイベントを取得できるようにす
-            Button button1 = (Button) findViewById(R.id.button1);
+            Button button1 = findViewById(R.id.button2);
             button1.setOnClickListener(new OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -35,12 +35,12 @@ public class MainActivity extends AppCompatActivity {
                         getLayoutInflater().inflate(R.layout.view2, layout);
                         count = 2;
                     } else if (count == 2) {
-                        LinearLayout layout =  findViewById(R.id.linearlayout2);
+                        LinearLayout layout =  findViewById(R.id.ConstraintLayout2);
                         layout.removeAllViews();
                         getLayoutInflater().inflate(R.layout.view3, layout);
                         count = 3;
                     }else if (count == 3) {
-                        LinearLayout layout =  findViewById(R.id.linearlayout3);
+                        LinearLayout layout =  findViewById(R.id.ConstraintLayout3);
                         layout.removeAllViews();
                         getLayoutInflater().inflate(R.layout.view2, layout);
                         count = 2;
@@ -48,89 +48,89 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
-        private MediaPlayer mediaPlayer;{
-          setContentView(R.layout.View2);
 
-          //音楽開始ボタン
-          Button buttonStart = findViewById(R.id.start);
-
-          //リスナーをボタンに登録
-          buttonStart.setOnClickListener(new OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  //音楽再生
-                  audioPlay();
-              }
-          });
-
-          //音楽停止ボタン
-          Button buttonStop = findViewById(R.id.stop);
-
-          //リスナーをボタンに登録
-          buttonStop.setOnClickListener(new OnClickListener() {
-              @Override
-              public void onClick(View v) {
-                  if (mediaPlayer != null) {
-                      //音楽停止
-                      audioStop();
-                  }
-              }
-          });
-        }
-
-        private boolean audioSetup() {
-          boolean fileCheck = false;
-
-          //rawに音声ファイルを置いた場合
-          mediaPlayer = MediaPlayer.create(this, R.raw.marbletechno2);
-          //音量調整を端末のボタンに任せる
-          setVolumeControlStream(AudioManager.STREAM_MUSIC);
-          fileCheck = true;
-
-          return fileCheck;
-        }
-
-        private void audioPlay() {
-          if (mediaPlayer == null) {
-              //audioファイルを読出し
-              if (audioSetup()) {
-                  Toast.makeText(getApplication(), "Rread audio file", Toast.LENGTH_SHORT).show();
-              } else {
-                Toast.makeText(getApplication(), "Error: read audio file", Toast.LENGTH_SHORT).show();
-                return;
-              }
-
-          } else {
-              //繰り返し再生する場合
-              mediaPlayer.stop();
-              mediaPlayer.reset();
-              //リソースの解放
-              mediaPlayer.release();
-          }
-          //再生する
-          mediaPlayer.start();
-
-          //終了を検知するリスナー
-          mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-              @Override
-              public void onCompletion(MediaPlayer mp) {
-                  Log.d("debug", "end of audio");
-                  audioStop();
-              }
-          });
-        }
-        private void audioStop(){
-          //再生終了
-          mediaPlayer.stop();
-          //リセット
-          mediaPlayer.reset();
-          //リソースの解放
-          mediaPlayer.release();
-
-          mediaPlayer = null;
-        }
-
-
+//        private MediaPlayer mediaPlayer;{
+//          setContentView(R.layout.view2)
+//          //音楽開始ボタン
+//          Button buttonStart = findViewById(R.id.start);
+//
+//          //リスナーをボタンに登録
+//          buttonStart.setOnClickListener(new OnClickListener() {
+//              @Override
+//              public void onClick(View v) {
+//                  //音楽再生
+//                  audioPlay();
+//              }
+//          });
+//
+//          //音楽停止ボタン
+//          Button buttonStop = findViewById(R.id.stop);
+//
+//          //リスナーをボタンに登録
+//          buttonStop.setOnClickListener(new OnClickListener() {
+//              @Override
+//              public void onClick(View v) {
+//                  if (mediaPlayer != null) {
+//                      //音楽停止
+//                      audioStop();
+//                  }
+//              }
+//          });
+//        }
+//
+//        private boolean audioSetup() {
+//          boolean fileCheck = false;
+//
+//          //rawに音声ファイルを置いた場合
+//          mediaPlayer = MediaPlayer.create(this, R.raw.marbletechno2);
+//          //音量調整を端末のボタンに任せる
+//          setVolumeControlStream(AudioManager.STREAM_MUSIC);
+//          fileCheck = true;
+//
+//          return fileCheck;
+//        }
+//
+//        private void audioPlay() {
+//          if (mediaPlayer == null) {
+//              //audioファイルを読出し
+//              if (audioSetup()) {
+//                  Toast.makeText(getApplication(), "Rread audio file", Toast.LENGTH_SHORT).show();
+//              } else {
+//                Toast.makeText(getApplication(), "Error: read audio file", Toast.LENGTH_SHORT).show();
+//                return;
+//              }
+//
+//          } else {
+//              //繰り返し再生する場合
+//              mediaPlayer.stop();
+//              mediaPlayer.reset();
+//              //リソースの解放
+//              mediaPlayer.release();
+//          }
+//          //再生する
+//          mediaPlayer.start();
+//
+//          //終了を検知するリスナー
+//          mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//              @Override
+//              public void onCompletion(MediaPlayer mp) {
+//                  Log.d("debug", "end of audio");
+//                  audioStop();
+//              }
+//          });
+//        }
+//        private void audioStop(){
+//          //再生終了
+//          mediaPlayer.stop();
+//          //リセット
+//          mediaPlayer.reset();
+//          //リソースの解放
+//          mediaPlayer.release();
+//
+//          mediaPlayer = null;
+//        }
+//
+//
 }
 
   // Log.v(tag:"init", "初期化しました。")
